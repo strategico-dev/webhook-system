0e14529688bfec8c16fd65d8fba80b7eb0dde065
El Strategico <strategico.dev@gmail.com>
2020-10-05 10:55:19 +0300

    Added method: findByName

--- RecordService/app/Models/WebhookEvent.php
+++ RecordService/app/Models/WebhookEvent.php
@@ -2,17 +2,32 @@
 
 namespace App\Models;
 
-use App\Models\AppClient_WebhookEvent;
+use App\Models\AppClient;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property $clients
+ */
 class WebhookEvent extends Model
 {
     protected $fillable = [
         'name'
     ];
 
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
     public function clients()
     {
-        return $this->belongsToMany(AppClient_WebhookEvent::class);
+        return $this->belongsToMany(AppClient::class);
+    }
+
+    /**
+     * @param string $name
+     * @return mixed
+     */
+    public static function findByName(string $name)
+    {
+        return self::where('name', '=', $name)->first();
     }
 }
