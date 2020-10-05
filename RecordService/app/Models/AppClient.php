cf2adbe804972494a75b87bb700431f39ded33a7
El Strategico <strategico.dev@gmail.com>
2020-10-05 10:54:48 +0300

    Added method: create

--- RecordService/app/Models/AppClient.php
+++ RecordService/app/Models/AppClient.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Models\WebhookEvent;
+use App\Models\AppClient_WebhookEvent;
 use Illuminate\Database\Eloquent\Model;
 
 class AppClient extends Model
@@ -10,4 +11,16 @@ class AppClient extends Model
     protected $fillable = [
         'name', 'url_handler'
     ];
+
+    /**
+     * @param array $attributes
+     * @return \Illuminate\Database\Eloquent\Builder|Model
+     */
+    public static function create(array $attributes = [])
+    {
+        $appClient = static::query()->create($attributes);
+        AppClient_WebhookEvent::create($appClient->id, $attributes['selected_event']);
+
+        return $appClient;
+    }
 }
