e0d1ebbbeab2098c492d0c87d285dfc23e95edef
El Strategico <strategico.dev@gmail.com>
2020-10-05 10:53:17 +0300

    Added logic for work with webhook facade

--- RecordService/app/Http/Controllers/RecordController.php
+++ RecordService/app/Http/Controllers/RecordController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\Record;
+use App\Facades\Webhook;
+use App\Models\WebhookEvent;
 use Illuminate\Http\Request;
 
 class RecordController extends Controller
 {
     public function store(Request $request)
     {
-        Record::create($request->input());
+        $record = Record::create($request->input());
+
+        Webhook::notify(
+            WebhookEvent::findByName('OnRecordSaved'),
+            $record->toArray()
+        );
 
         return redirect('/records');
     }
